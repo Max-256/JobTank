2688d006aa1744ed4d213657e15b4fe5bc1b183d
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-02-07 17:23:17 +0300

    build the http service

---
+++ src/services/httpService.js
@@ -0,0 +1,24 @@
+
+import axios from 'axios';
+import {toast} from 'react-toastify';
+
+axios.interceptors.response.use(null, error => {
+    const expectedError = error.response && error.response.status >= 400 && error.response.status < 500;
+
+    if(!expectedError)
+        toast.error('Sorry! An unexpected error ocurred');
+    
+    return Promise.reject(error);    
+});
+
+function setJwt(jwt){
+    axios.defaults.headers.common['x-auth-token'] = jwt;
+}
+
+export default {
+    get: axios.get,
+    post: axios.post,
+    put: axios.put,
+    delete: axios.delete,
+    setJwt
+}
