ed2ba262771c1420fae87b59701804a144390779
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-02-08 08:44:25 +0300

    connect register to recruiter api

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import Joi from 'joi-browser';
 import useForm from '../hooks/useForm';
 import { Form } from 'react-bootstrap';
+import {toast} from 'react-toastify';
+import { register  } from '../services/recruiter';
 
 function Register(props) {
     const formData = {
@@ -17,8 +19,15 @@ function Register(props) {
         password: Joi.string().trim().min(5).max(50).required()
     };
 
-    const doSubmit = () => {
-        console.log(data);
+    const doSubmit = async () => {
+        try{
+            const response = await register(data);
+            console.log(response.data);
+        }
+        catch(ex){
+            if(ex.response && ex.response.status === 400)
+            toast.error(ex.response.data);
+        }     
     }
 
     const {data, handleSubmit, renderInput, renderButton} = 
---
+++ src/services/recruiter.js
@@ -0,0 +1,12 @@
+
+import http from './httpService';
+import config from '../config.json';
+const apiEndPoint = config.apiUrl + "/recruiters";
+
+export function register(recruiter) {
+    return http.post(apiEndPoint, {
+        username: recruiter.username,
+        email: recruiter.email,
+        password: recruiter.password
+    });
+}
